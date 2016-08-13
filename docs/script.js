bebdb14c97fbf30d0186baffff482e7853d956a6
thiagotoledo <javaephp@gmail.com>
2016-08-12 22:24:17 -0300

    Update docs

--- docs/script.js
+++ docs/script.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
             projectFullName = $(xml).find('file').children('class').children('full_name').text();
             projectDescription = $(xml).find('file').children('class').children('docblock').children('description').text();
             projectTags = $(xml).find('file').children('class').children('docblock').children('tag');
+            methods = $(xml).find('file').children('class').children('method');
 
             $("#projectNameLink").text(projectName);
             $("title").text(projectName);
@@ -35,6 +36,65 @@ $(document).ready(function () {
                 $("#projectTags").append("<li class='projectTag'><a " + tagLink + " >" + tagName + "</a> " + tagDescription +"</li>");
 
             })
+
+            var htmlPublicMethods = "";
+            var htmlMethodsBlock = "";
+            i = 0;
+            methods.each(function(){
+
+                var method = $(this);
+                var visibility = method.attr("visibility");
+                var methodName = method.children('name').text();
+                var methodFullName = method.children('full_name').text();
+                var methodDescription = method.children('docblock').children('description').text();
+                var methodTags = method.children('docblock').children('tag');
+
+                //Public Methods links - top
+                if(visibility==="public"){
+
+                    if(i != 0){
+                        htmlPublicMethods += ", ";
+                    }
+
+                    htmlPublicMethods += "<a href='#" + methodName + "' class='public_method_link' >" + methodName + "</a>";
+
+                    i++;
+                }
+
+                //methodsBlock
+                if(visibility==="public"){
+
+                    var methodReturn = "void";
+
+                    htmlMethodsBlock += "<div class='methodElement'>";
+                        htmlMethodsBlock += "<h2 class='methodElementName'>" + methodName + " <a name='" + methodName + "'></a></h2>";
+                        htmlMethodsBlock += "<p class='methodElementNameFullName'>" + methodFullName + "</p>";
+                        htmlMethodsBlock += "<p class='methodDescription'>" + methodDescription + "</p>";
+                        htmlMethodsBlock += "<ul>";
+
+                    methodTags.each(function(){
+                            var tag = $(this);
+                            var tagName = tag.attr("name");
+                            var tagType = tag.attr("type");
+
+                            if(tagName == "param") {
+                                var tagVariable = tag.attr("variable");
+                                htmlMethodsBlock += "<li>Param: <span class='methodDescriptionParamType'>" + tagType + "</span> <b class='methodDescriptionParamName' >" + tagVariable + "</b></li>";
+                            }else{
+                                methodReturn = tagType;
+                            }
+                    });
+
+                        htmlMethodsBlock += "</ul>";
+                        htmlMethodsBlock += "<p>Return: <span class='methodDescriptionParamType'>" + methodReturn + "</span></p>";
+                    htmlMethodsBlock += "</div>";
+
+                }
+
+            });
+
+            $("#publicMethods").append(htmlPublicMethods);
+            $("#methodsBlock").append(htmlMethodsBlock);
 
 
         },
